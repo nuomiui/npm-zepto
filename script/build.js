f0f862901fa79c848d71832b2767a5f6bb8d02b1
memoryza <jincai.wang@foxmail.com>
2017-04-11 18:13:00 +0800

    test

--- script/build.js
+++ script/build.js
@@ -7,7 +7,7 @@ function abs(name) {
 	return path.join(dir, '../', name);
 }
 
-child_process.execSync('cp -R ' + abs('node_modules/zepto') + ' zepto');
+child_process.execSync('cp -R ' + abs('node_modules/zepto') + ' ' + abs('zepto'));
 if (os.platform() === 'win32') {
 	if (fs.existsSync(abs('index.js'))) {
 		fs.unlink(abs('index.js'));
